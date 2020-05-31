137f30b9335a14c4e8fded518b0c3306f976b4be
Vamsi <guggilamvamsi@email.com>
2020-05-31 20:00:38 +0530

    commited

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>About Us</h1>
+    <p>We are group of developers,Here we share you solutions for some of the challenges we face.</p>
+    
+    <p>Thank You</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      
+    </div>
+  </Layout>
+)
+
+export default IndexPage
